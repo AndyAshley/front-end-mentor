7c2a11e58239ce898e4879a4ac62c90d31a27079
AndyAshley <AndrewAshley@frazer.biz>
2022-03-21 23:23:39 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+const IDBOX = document.querySelector(".id-box");
+const ADVICEBOX = document.querySelector(".advice-box");
+
+async function getAdvice() {
+  const response = await fetch("https://api.adviceslip.com/advice");
+  const data = await response.json();
+  const advice = data.slip.advice;
+  const adviceId = data.slip.id;
+
+  IDBOX.innerHTML = adviceId;
+  ADVICEBOX.innerHTML = advice;
+
+  console.log(adviceId, advice);
+}
+
+getAdvice();
