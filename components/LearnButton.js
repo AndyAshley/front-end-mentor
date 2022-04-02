14ef1789d6a5d5f51263f41849cf8b24b216b39a
AndyAshley <AndrewAshley@frazer.biz>
2022-04-02 02:39:42 -0400

    shit

--- components/LearnButton.js
+++ components/LearnButton.js
@@ -4,7 +4,7 @@ import styles from "../components/css/LearnButton.module.css";
 export default function LearnButton(props) {
   return (
     <div role="button">
-      <strong className="position-relative" style={{ fontFamily: "Fraunces", fontWeight: "900" }}>
+      <strong className="position-relative" style={{ fontFamily: "Fraunces", fontWeight: "900", zIndex: "2" }}>
         LEARN MORE
         <span className={classNames(styles.LearnSpan, "position-absolute opacity-25")} style={{ background: props.underline, width: "115%", borderRadius: "10px", zIndex: "-1" }}></span>
       </strong>
