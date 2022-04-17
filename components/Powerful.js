33e2460ff1e21ee7b98973d831dfd93922dc2623
AndyAshley <AndrewAshley@frazer.biz>
2022-04-17 01:06:23 -0400

    dfn

--- components/Powerful.js
+++ components/Powerful.js
@@ -19,21 +19,45 @@ export default function Introduction() {
     //   </div>
     // );
 
-    <div className={classNames(styles.IntroductionWrapper, "container-fluid text-center")}>
-      <div className="row flex-column flex-lg-row align-items-center position-relative">
-        <div className={classNames(styles.DesktopImage, "col-12 col-lg-6 px-0 order-lg-1")}>
-          <img src="/images/illustration-laptop-mobile.svg" alt="editor Illustration" className="d-block d-lg-none img-fluid" />
+    // <div className={classNames(styles.IntroductionWrapper, "container-fluid text-center")}>
+    //   <div className="row flex-column flex-lg-row align-items-center position-relative">
+    //     <div className={classNames(styles.DesktopImage, "col-12 col-xxl-6 px-0 position-relative order-lg-1")}>
+    //       <img src="/images/illustration-laptop-mobile.svg" alt="editor Illustration" className="d-block d-lg-none img-fluid" />
+    //       <img src="/images/illustration-laptop-desktop.svg" alt="editor Illustration" className="d-none d-lg-block" />
+    //     </div>
+    //     <div className="col-12 col-lg-6 d-flex order-lg-2 flex-column justify-content-center text-lg-start">
+    //       <div className="container pt-3 pt-xxl-4">
+    // <h2 className={styles.SubTitle}>Free, open, simple</h2>
+    // <p>
+    //   Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works
+    //   seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.
+    // </p>
+    //       </div>
+    //       <div className="container pt-lg-5">
+    // <h2 className={classNames(styles.SubTitle, "pt-4")}>Powerful tooling</h2>
+    // <p>Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div className={styles.PowerfulWrapper}>
+      <div className={styles.ImageWrapper}>
+        <div className={styles.ImageContainer}>
+          <img src="/images/illustration-laptop-mobile.svg" alt="editor Illustration" className="d-block d-lg-none" />
           <img src="/images/illustration-laptop-desktop.svg" alt="editor Illustration" className="d-none d-lg-block" />
         </div>
-        <div className="col-12 col-lg-6 d-flex order-lg-2 flex-column justify-content-center text-lg-start">
-          <div className="container pt-3 pt-lg-4">
+      </div>
+      <div className={styles.TextWrapper}>
+        <div className={styles.TextContainer}>
+          <div className="container">
             <h2 className={styles.SubTitle}>Free, open, simple</h2>
             <p>
               Blogr is a free and open source application backed by a large community of helpful developers. It supports features such as code syntax highlighting, RSS feeds, social media integration, third-party commenting tools, and works
               seamlessly with Google Analytics. The architecture is clean and is relatively easy to learn.
             </p>
           </div>
-          <div className="container pt-lg-5">
+          <div className="container">
             <h2 className={classNames(styles.SubTitle, "pt-4")}>Powerful tooling</h2>
             <p>Batteries included. We built a simple and straightforward CLI tool that makes customization and deployment a breeze, but capable of producing even the most complicated sites.</p>
           </div>
